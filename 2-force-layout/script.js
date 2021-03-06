00b8ff296ae63ea999129fa90154daf389623f93
Yuriko Anzai Schumacher <yuriko.schumacher@gmail.com>
2021-03-05 21:17:11 -0500

    class

--- 2-force-layout/script.js
+++ 2-force-layout/script.js
@@ -1,18 +1,95 @@
-const margin = {t: 50, r:50, b: 50, l: 50};
-const size = {w: 800, h: 800};
-const svg = d3.select('svg');
+const margin = { t: 50, r: 50, b: 50, l: 50 };
+const size = { w: 800, h: 800 };
+const svg = d3.select("svg");
 
-svg.attr('width', size.w)
-    .attr('height', size.h);
+svg.attr("width", size.w).attr("height", size.h);
 
-const containerG = svg.append('g').classed('container', true);
+const containerG = svg.append("g").classed("container", true);
 
-d3.json('data/data.json')
-.then(function(data) {
-    console.log(data);
+d3.json("data/data.json").then(function (data) {
+	console.log(data);
 
+	let scaleLinear = d3
+		.scaleLinear()
+		.domain(d3.extent(data.links, (d) => d.value))
+		.range([0.1, 1]);
+
+	let scaleOpacity = d3
+		.scaleLinear()
+		.domain(d3.extent(data.links, (d) => d.value))
+		.range([0.1, 1]);
+
+	let simulation = d3
+		.forceSimulation(data.nodes)
+		.force("center", d3.forceCenter(size.w / 2, size.h / 2))
+		.force("charge", d3.forceManyBody().strength(-20))
+		.force(
+			"link",
+			d3
+				.forceLink(data.links)
+				.id((d) => d.id)
+				.strength((d) => scaleLinear(d.value))
+		);
+
+	let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+	let lines = containerG
+		.selectAll("line")
+		.data(data.links)
+		.join("line")
+		.attr("stroke", "black")
+		.attr("stroke-width", 1)
+		.attr("opacity", (d) => scaleOpacity(d.value))
+		.attr("x1", (d) => Math.random() * size.w)
+		.attr("x2", (d) => Math.random() * size.w)
+		.attr("y1", (d) => Math.random() * size.h)
+		.attr("y2", (d) => Math.random() * size.h);
+
+	let nodes = containerG
+		.selectAll("circle")
+		.data(data.nodes)
+		.join("circle")
+		.attr("r", 5)
+		.attr("cx", (d) => Math.random() * size.w)
+		.attr("cy", (d) => Math.random() * size.h)
+		.style("fill", (d) => colorScale(d.group))
+		.call(drag(simulation));
+
+	simulation.on("tick", function () {
+		console.log("tick");
+
+		nodes.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+
+		lines
+			.attr("x1", (d) => d.source.x)
+			.attr("y1", (d) => d.source.y)
+			.attr("x2", (d) => d.target.x)
+			.attr("y2", (d) => d.target.y);
+	});
 });
 
-
 function drag(simulation) {
+	let dragStarted = function (event) {
+		console.log(event);
+		if (!event.active) simulation.alphaTarget(0.3).restart();
+		event.subject.fx = event.x; // fx and fy: fixed x and fixed y
+		event.subject.fy = event.y;
+	};
+
+	let dragged = function (event) {
+		event.subject.fx = event.x;
+		event.subject.fy = event.y;
+	};
+
+	let dragEnded = function (event) {
+		if (!event.active) simulation.alphaTarget(0);
+		event.subject.fx = null;
+		event.subject.fy = null;
+	};
+
+	return d3
+		.drag()
+		.on("start", dragStarted)
+		.on("drag", dragged)
+		.on("end", dragEnded);
 }
--- 3-force-bubbles/script.js
+++ 3-force-bubbles/script.js
@@ -1,61 +1,98 @@
-const margin = {t: 50, r:50, b: 50, l: 50};
-const size = {w: 800, h: 800};
-const svg = d3.select('svg');
+const margin = { t: 50, r: 50, b: 50, l: 50 };
+const size = { w: 800, h: 800 };
+const svg = d3.select("svg");
 
-svg.attr('width', size.w)
-    .attr('height', size.h);
+svg.attr("width", size.w).attr("height", size.h);
 
-const containerG = svg.append('g').classed('container', true);
+const containerG = svg.append("g").classed("container", true);
 
-d3.json('data/data.json')
-.then(function(data) {
-    console.log(data);
-    data.nodes.forEach(d => { d.value = 5 + Math.random()*10; });
+d3.json("data/data.json").then(function (data) {
+	console.log(data);
+	data.nodes.forEach((d) => {
+		d.value = 5 + Math.random() * 10;
+	});
 
-    let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+	let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+	let scaleX = d3
+		.scaleBand()
+		.domain([...Array(11).keys()])
+		.range([margin.l, size.w - margin.r]);
 
-    let simulation = d3.forceSimulation(data.nodes)
+	let objMax = {};
 
-    let node = svg.append('g')
-        .attr('stroke', '#fff')
-        .attr('stroke-width', 1.5)
-        .selectAll('circle')
-        .data(data.nodes)
-        .join('circle')
-        .attr('yy', d => d.group)
-        .attr('r', d => d.value)
-        .attr('fill', d => colorScale(d.group))
-        .call(drag(simulation));
+	let groups = new Set(data.nodes.map((d) => d.group));
+	groups = Array.from(groups);
 
-    simulation.on('tick', () => {
-        node
-            .attr('cx', d => d.x)
-            .attr('cy', d => d.y);
-        });
+	groups.forEach((groupNo) => {
+		let groupData = data.nodes.filter((d) => +d.group === groupNo);
+		let maxVal = d3.max(groupData, (d) => d.value);
+		objMax["g" + groupNo] = maxVal;
+	});
+	console.log(objMax);
+
+	data.nodes.forEach((node) => {
+		if (objMax["g" + node.group] === node.value) {
+			// node with max value in group
+			node.fx = scaleX(node.group);
+			node.fy = scaleX(node.group);
+		}
+	});
+
+	let simulation = d3
+		.forceSimulation(data.nodes)
+		// .force('center', )
+		.force(
+			"collide",
+			d3.forceCollide().radius((d) => d.value)
+		)
+		.force("charge", d3.forceManyBody().strength(0.3))
+		.force(
+			"x",
+			d3
+				.forceX()
+				.x((d) => scaleX(d.group))
+				.strength(0.5)
+		)
+		.force("y", d3.forceY().y(size.h / 2));
+
+	let node = svg
+		.append("g")
+		.attr("stroke", "#fff")
+		.attr("stroke-width", 1.5)
+		.selectAll("circle")
+		.data(data.nodes)
+		.join("circle")
+		.attr("yy", (d) => d.group)
+		.attr("r", (d) => d.value)
+		.attr("fill", (d) => colorScale(d.group))
+		.call(drag(simulation));
+
+	simulation.on("tick", () => {
+		node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+	});
 });
 
-
 function drag(simulation) {
-  
-    function dragstarted(event) {
-      if (!event.active) simulation.alphaTarget(0.3).restart();
-      event.subject.fx = event.subject.x;
-      event.subject.fy = event.subject.y;
-    }
-    
-    function dragged(event) {
-      event.subject.fx = event.x;
-      event.subject.fy = event.y;
-    }
-    
-    function dragended(event) {
-      if (!event.active) simulation.alphaTarget(0);
-      event.subject.fx = null;
-      event.subject.fy = null;
-    }
-    
-    return d3.drag()
-        .on('start', dragstarted)
-        .on('drag', dragged)
-        .on('end', dragended);
-  }
+	function dragstarted(event) {
+		if (!event.active) simulation.alphaTarget(0.3).restart();
+		event.subject.fx = event.subject.x;
+		event.subject.fy = event.subject.y;
+	}
+
+	function dragged(event) {
+		event.subject.fx = event.x;
+		event.subject.fy = event.y;
+	}
+
+	function dragended(event) {
+		if (!event.active) simulation.alphaTarget(0);
+		event.subject.fx = null;
+		event.subject.fy = null;
+	}
+
+	return d3
+		.drag()
+		.on("start", dragstarted)
+		.on("drag", dragged)
+		.on("end", dragended);
+}
